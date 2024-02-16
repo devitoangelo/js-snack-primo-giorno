b4d35a92cec20155b2e1be33f6a8385cacc0c9df
devitoangelo <angelodevito4@gmail.com>
2024-02-16 16:30:49 +0100

    Esercizio 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,24 @@ console.log(number);
 
 const PI = "3.14";
 console.log(PI);
+
+
+//Esercizio 2/B
+
+//Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+
+/*
+- const
+- number
+-console
+
+Non possiamo usare const con variante chiamata PI perchè è stata gia usata in precedenza ( non possiamo usare const perchè è una variabile che non si può riasseganre come per esempio " let e var" se proviamo a cambiare o a riscrivere il suo valore avremmo un errore in console)
+*/
+
+
+const PI = "3.1416."
+console.log(PI)
+
+
+
